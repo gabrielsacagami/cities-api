78a88b60d2ec98787b68704465507c3950796f0f
gabrielsacagami <gabrieldnzs@gmail.com>
2023-02-10 09:55:26 -0300

    Add initial CRUD

---
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models;
+
+public class PointOfInterestForCreationDto
+{
+    [Required(ErrorMessage = "You should provide a name value")]
+    [MaxLength(50, ErrorMessage = "The max lenght is 50 characters")]
+    public string Name { get; set; } = string.Empty;
+    [MaxLength(200, ErrorMessage = "The max lenght is 200 characters")]
+    public string? Description { get; set; }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -7,7 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
-}).AddXmlDataContractSerializerFormatters();
+}).AddNewtonsoftJson()
+.AddXmlDataContractSerializerFormatters();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
